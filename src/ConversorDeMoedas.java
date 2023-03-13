f134b01d902102970dbe8ffee6601141475685a4
Igor Zorzela <igorzorzela@gmail.com>
2023-03-13 14:15:43 -0300

    Update ConversorDeMoedas.java

--- src/ConversorDeMoedas.java
+++ src/ConversorDeMoedas.java
@@ -1,5 +1,4 @@
 import java.text.DecimalFormat;
-
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 
@@ -16,20 +15,25 @@ public class ConversorDeMoedas {
         boolean continuar = true;
 
         while(continuar){
-            String escolha = (String) JOptionPane.showInputDialog(null, "Esolha uma opção", "Conversor de moedas",
-                    JOptionPane.QUESTION_MESSAGE, null,
-                    opcoes,
-                    opcoes);
+            //mostra as opções de conversão
+            String escolha = (String) JOptionPane.showInputDialog(null, "Esolha uma opção", "Conversor de moedas",JOptionPane.QUESTION_MESSAGE, null, opcoes, opcoes);
 
+            //verifica se foi selecionado
             if(escolha != null){
+                //mostra o campo para inserir
+                //o valor a ser convertido
                 String valor = JOptionPane.showInputDialog(null, "Digite o valor que deseja converter:", "Conversor de moedas", JOptionPane.PLAIN_MESSAGE);
 
+                //verifica se foi inseirdo um valor válido
+                //e se o campo não ficou vazio
                 if(valor != null && !valor.isEmpty()){
                     double resultado = Double.parseDouble(valor);
 
                     double valorMoeda = 0.0;
                     String simbolo = "";
 
+                    //depois cai nesse switch onde vai ler qual foi
+                    //as moedas que o usuario escolheu converter
                     switch (escolha) {
                         case "Real para Dólar" -> {
                             valorMoeda = 0.19;
@@ -104,8 +108,10 @@ public class ConversorDeMoedas {
                         }                                                                                         
                         
                     }
+                    //formata o número
                     DecimalFormat df = new DecimalFormat("0.00");
 
+                    //calcula o valor da conversão
                     double valorFinal = (resultado * valorMoeda);
                     JOptionPane.showMessageDialog(null, "O valor da conversão é de " + simbolo + (df.format(valorFinal)), "Conversor de moedas", JOptionPane.INFORMATION_MESSAGE);
                 }
@@ -113,6 +119,9 @@ public class ConversorDeMoedas {
                 JOptionPane.showMessageDialog(null, "Valor inválido", "Conversor de moedas", JOptionPane.ERROR_MESSAGE);
             }
 
+            //no final verifica se u usuario
+            //deseja fazer uma nova conversão
+            // ou sair da aplicação
             JDialog.setDefaultLookAndFeelDecorated(true);
             int resposta = JOptionPane.showConfirmDialog(null, "Deseja continuar?", "Confirmação",
                     JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
